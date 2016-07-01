2cee5daaedb0e2377f82791463144af46d76b61b
Ray Xiao <rxiao@amgen.com>
2016-06-30 20:33:45 -0700

    fix bug

--- funsets/src/main/scala/funsets/Main.scala
+++ funsets/src/main/scala/funsets/Main.scala
@@ -3,4 +3,7 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+
+  def evenNumbers: Set = x => x % 2 == 0
+  println(printSet(evenNumbers))
 }
